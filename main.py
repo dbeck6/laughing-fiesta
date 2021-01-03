798b758d173292eb2921c4ab4585c567867c8f06
Rob Weddell <rweddell4@gmail.com>
2021-01-03 10:57:38 -0600

    refactored main.py and created main_window.py

--- main.py
+++ main.py
@@ -1,46 +1,9 @@
-from tkinter import *
-
-def handle_keypress(event):
-    print(event.char)
-
-def handle_click(event):
-    print('The button was clicked')
+from main_window import MainWindow
 
 def main():
-    window = Tk()
-    event_list = []
-    message = Label(text='There is something here ...')
-    message.pack()
-    frame = Frame(window) 
-    frame.pack() 
-    bottomframe = Frame(window) 
-    bottomframe.pack(side=BOTTOM) 
-    redbutton = Button(
-        frame, 
-        text='Red', 
-        fg ='red'
-    ) 
-    redbutton.pack(side=LEFT) 
-    bluebutton = Button(
-        frame, 
-        text='Blue', 
-        fg='blue'
-    ) 
-    bluebutton.pack(side=LEFT) 
-    blackbutton = Button(
-        bottomframe, 
-        text='Black', 
-        fg='black'
-    ) 
-    blackbutton.pack(side=BOTTOM) 
-    window.mainloop() 
-    window.bind('<Key>', handle_keypress)
-    window.bind('<Button-1>', handle_click)
-    window.mainloop()
+   window = MainWindow()
+   window.run()
     
-class MainWindow():
-    def __init__():
-        self.
 
 if __name__ == '__main__':
     main()
--- main_window.py
+++ main_window.py
@@ -5,29 +5,32 @@ class MainWindow():
 
     def __init__(self):
         self.window = Tk(className='Laughing Fiesta')
+        self.window.geometry("300x200")
+        
         self.name_list = []
 
         self.name_collector = Entry(self.window)
         self.name_collector.pack()
 
-        self.add_name_button = Button(self.window, text='Add name', command = self.collect_name)
+        self.add_name_button = Button(self.window, text='Add name', command=self.collect_name)
         self.add_name_button.pack()
+
+        self.choose_name_button = Button(self.window, text='Pick name', command=self.select_name)
+        self.choose_name_button.pack()
 
     def collect_name(self):
         new_name = self.name_collector.get()
         self.name_list.append(new_name)
-        self.name_collector.delete(0)
+        name_length = len(new_name)
+        self.name_collector.delete(0, name_length)
 
     def select_name(self):
-        chosen_name = choice(self.name_list)
-        self.name_list.pop(chosen_name)
-        label = Label(self.window, text=chosen_name)
-        label.pack()
+        if len(self.name_list) > 0:
+            chosen_name = choice(self.name_list)
+            if len(chosen_name) > 0:
+                self.name_list.remove(chosen_name)
+                label = Label(self.window, text=chosen_name)
+                label.pack()
 
     def run(self):
         self.window.mainloop()
-
-
-w = MainWindow()
-
-w.run()
